2d63b3e741c7118a7056f1616100684f1948aaa1
alikafy <kafy_ali@yahoo.com>
2025-08-21 03:50:26 +0330

    fix bug in response

--- vulnerabilities/api/views.py
+++ vulnerabilities/api/views.py
@@ -74,9 +74,9 @@ class CalculateView(APIView):
         for asset in vulnerability.impacted_assets.all():
             if not asset.is_active:
                 continue
-            response = vulnerability.response_set.filter(impacted_asset=asset)
-            agent_response = response.first().agent_response if response.exists() else AgentCalculator(vulnerability, asset, agent_model).calculate()
-            rule_base_response = response.first().rule_response if response.exists() else rule_base_answer(asset, vulnerability)
+            response = vulnerability.response_set.filter(impacted_asset=asset).first()
+            agent_response = response.agent_response if response and response.agent_response else AgentCalculator(vulnerability, asset, agent_model).calculate()
+            rule_base_response = response.rule_response if response and response.rule_response else rule_base_answer(asset, vulnerability)
             values.append({'agent_response': agent_response, 'rule_base_response': rule_base_response, 'asset': AssetSerializer(asset).data})
         response = {'vulnerability_id': vulnerability.id, 'values': values}
         return Response(response, status=status.HTTP_200_OK)
